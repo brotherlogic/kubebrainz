c68e9a7abb87f1ba8e2963840205b604d65db221
Simon Tucker <brotherlogic@gmail.com>
2025-11-24 22:02:21 -0800

    Adds initial download step (#4)

---
+++ downloader.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+const (
+	BASE = "https://data.metabrainz.org/pub/musicbrainz/data/fullexport/"
+)
+
+func getLatest() (string, error) {
+	// Create a new bytes.Buffer
+	var buf bytes.Buffer
+	resp, err := http.Get(fmt.Sprintf("%vLATEST", BASE))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	_, err = io.Copy(&buf, resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+func downloadFile() error {
+	latest, err := getLatest()
+	if err != nil {
+		return err
+	}
+
+	out, err := os.Create("download.tar.bz2")
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	resp, err := http.Get(fmt.Sprintf("%v%v/mdbdump.tar.bz2", BASE, latest))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
