8a3b077a53757598d815a263faaf7d69d223f7b3
Simon Tucker <brotherlogic@gmail.com>
2026-05-02 20:58:02 -0700

    fix: update branch pattern to match slashes in GH actions (#52)

--- start-tmux.sh
+++ start-tmux.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Ensure UTF-8 locale
+export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
+
 # Ensure the 'prod' session exists
 if ! tmux has-session -t prod 2>/dev/null; then
   # Create a new session named 'prod', detached
-  tmux -u new-session -d -s prod
+  tmux -u new-session -A -d -s prod
   
   # Split the window horizontally (-h)
   # The left pane will remain a terminal
